c384f412e99fb0e7c6b669e291fd6b7a48c51040
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2025-05-08 22:45:53 +0530

    array

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 console.log("ICET");
+let customerList =[
+    {
+        id:1,
+        name:"saman",
+        email:"saman@gmail.com"
+
+    },
+    {
+        id:1,
+        name:"saman",
+        email:"saman@gmail.com"
+    },
+    {
+        id:1,
+        name:"saman",
+        email:"saman@gmail.com"
+
+    },
+    {
+        {
+        id:1,
+        name:"saman",
+        email:"saman@gmail.com"
+
+    },
+    }
+]
