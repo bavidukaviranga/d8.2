2e42a65c3f0058085216a67b027ef4ae3ccba2cc
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2025-05-10 12:42:32 +0530

    function is clearly saw

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("ICET");
+// console.log("ICET");
 let customerList =[
     {
         id:1,
@@ -12,17 +12,34 @@ let customerList =[
         email:"kamal@gmail.com"
     },
     {
-        id:1,
+        id:2,
         name:"nimal",
         email:"nimal@gmail.com"
 
     },
     {
-        {
-        id:1,
+        
+        id:3,
         name:"sunimal",
         email:"sunimal@gmail.com"
 
     },
-    }
+    
 ]
+let tblCustomer = document.getElementById("tblCustomer");
+
+///////////function clear//////////////////
+
+let loop=(customer)=>{
+   tblCustomer.innerHTML+=`
+    <tr>
+        <th>iD</th>
+        <th>Name</th>
+        <th>Email</th>
+     </tr>` 
+}
+customerList.forEach(loop)
+
+/////////////////////////////
+
+console.log(tblCustomer);
